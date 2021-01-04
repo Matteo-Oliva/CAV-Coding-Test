8aa5f8a8be62ed2fd5ebee7c95c0729de8dfe394
Matteo Oliva <olivamatteus@gmail.com>
2021-01-04 09:36:38 +0100

    add jokes Service for fecth data from api

--- Cav-Coding-Test/src/app/modules/shared/services/jokes/jokes.service.ts
+++ Cav-Coding-Test/src/app/modules/shared/services/jokes/jokes.service.ts
@@ -1,11 +1,20 @@
 import { HttpClient } from '@angular/common/http';
+import { IJokes } from './../../../../shared/models/interface/jokes';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JokesService {
-  jokesUrl = 'api/jokes'
+  jokesUrl = 'http://api.icndb.com/jokes/random'
   constructor(private http: HttpClient) { }
+
+
+  getJokes(): Observable<IJokes> {
+    return this.http.get<IJokes>(this.jokesUrl)
+
+  }
 }
 
