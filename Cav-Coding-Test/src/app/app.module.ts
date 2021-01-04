2e12a9f649b573a18f718672310cfc3db422b57e
Matteo Oliva <olivamatteus@gmail.com>
2021-01-04 09:35:37 +0100

    Added http clientModule

--- Cav-Coding-Test/src/app/app.module.ts
+++ Cav-Coding-Test/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 @NgModule({
@@ -11,7 +12,8 @@ import { NgModule } from '@angular/core';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
